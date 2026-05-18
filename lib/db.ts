e636ff23c1aab2836b644b18e76a135ff9657b38
dariagerwork-hash <daria.gerwork@gmail.com>
2026-05-18 15:58:08 +0700

    fix: switch from @vercel/kv to @upstash/redis

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,9 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
+
+const kv = new Redis({
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
+});
 import { v4 as uuidv4 } from "uuid";
 
 export interface Lead {
